3b27f9934ae47dd991a33612bb64104c97706920
Nicola Peditto <n.peditto@gmail.com>
2016-10-18 17:55:22 +0200

    Update database_cw.py

--- database_cw.py
+++ database_cw.py
@@ -1,8 +1,6 @@
 #
 # Copyright 2013 IBM Corp
 #
-# Author: Tong Li <litong01@us.ibm.com>
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -14,15 +12,15 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from oslo.utils import timeutils
+
+from oslo_log import log
+from oslo_utils import timeutils
 
 from ceilometer import dispatcher
-from ceilometer.openstack.common.gettextutils import _
-from ceilometer.openstack.common import log
+from ceilometer.event.storage import models
+from ceilometer.i18n import _LE, _LW
 from ceilometer.publisher import utils as publisher_utils
 from ceilometer import storage
-
-import json
 
 LOG = log.getLogger(__name__)
 
@@ -36,30 +34,55 @@ class DatabaseDispatcher(dispatcher.Base):
     To enable this dispatcher, the following section needs to be present in
     ceilometer.conf file
 
-    dispatchers = database
+    [DEFAULT]
+    dispatcher = database
     """
     def __init__(self, conf):
         super(DatabaseDispatcher, self).__init__(conf)
-        self.storage_conn = storage.get_connection_from_config(conf)
+
+        self._meter_conn = self._get_db_conn('metering', True)
+        self._event_conn = self._get_db_conn('event', True)
+
+    def _get_db_conn(self, purpose, ignore_exception=False):
+        try:
+            return storage.get_connection_from_config(self.conf, purpose)
+        except Exception as err:
+            params = {"purpose": purpose, "err": err}
+            LOG.exception(_LE("Failed to connect to db, purpose %(purpose)s "
+                              "re-try later: %(err)s") % params)
+            if not ignore_exception:
+                raise
+
+    @property
+    def meter_conn(self):
+        if not self._meter_conn:
+            self._meter_conn = self._get_db_conn('metering')
+
+        return self._meter_conn
+
+    @property
+    def event_conn(self):
+        if not self._event_conn:
+            self._event_conn = self._get_db_conn('event')
+
+        return self._event_conn
 
     def record_metering_data(self, data):
         # We may have receive only one counter on the wire
         if not isinstance(data, list):
             data = [data]
 
+	#LOG.info("CW -> SECRET PW %s", self.conf.publisher.telemetry_secret)
         for meter in data:
-            LOG.debug(_(
+            LOG.debug(
                 'metering data %(counter_name)s '
-                'for %(resource_id)s @ %(timestamp)s: %(counter_volume)s')
-                % ({'counter_name': meter['counter_name'],
-                    'resource_id': meter['resource_id'],
-                    'timestamp': meter.get('timestamp', 'NO TIMESTAMP'),
-                    'counter_volume': meter['counter_volume']}))
-
-
+                'for %(resource_id)s @ %(timestamp)s: %(counter_volume)s',
+                {'counter_name': meter['counter_name'],
+                 'resource_id': meter['resource_id'],
+                 'timestamp': meter.get('timestamp', 'NO TIMESTAMP'),
+                 'counter_volume': meter['counter_volume']})
             if publisher_utils.verify_signature(
-                    meter,
-                    self.conf.publisher.metering_secret):
+                    meter, self.conf.publisher.telemetry_secret):
                 try:
                     # Convert the timestamp to a datetime instance.
                     # Storage engines are responsible for converting
@@ -69,25 +92,46 @@ class DatabaseDispatcher(dispatcher.Base):
                         meter['timestamp'] = timeutils.normalize_time(ts)
 
 		    if meter['resource_metadata'].get('nested_uuid'):
-				before_uuid=meter['resource_id']
-				nested_uuid=meter['resource_metadata'].get("nested_uuid")
-				meter['resource_id'] = str(nested_uuid)
-				LOG.info("CW -> TUNNELLED METRIC from %s to %s", before_uuid, meter['resource_id'])	
-		    
+			before_uuid=meter['resource_id']
+			nested_uuid=meter['resource_metadata'].get("nested_uuid")
+			meter['resource_id'] = str(nested_uuid)
+			LOG.info("CW -> TUNNELLED METRIC from %s to %s", before_uuid, meter['resource_id'])
 
-                    self.storage_conn.record_metering_data(meter)
+
+                    self.meter_conn.record_metering_data(meter)
                 except Exception as err:
-                    LOG.exception(_('Failed to record metering data: %s'),
+                    LOG.exception(_LE('Failed to record metering data: %s'),
                                   err)
+                    # raise the exception to propagate it up in the chain.
+                    raise
             else:
-                LOG.warning(_(
+                LOG.warning(_LW(
                     'message signature invalid, discarding message: %r'),
                     meter)
 
     def record_events(self, events):
 
 	LOG.info('CW -> DATABASE record events %s', events)
+
         if not isinstance(events, list):
             events = [events]
 
-        return self.storage_conn.record_events(events)
+        event_list = []
+        for ev in events:
+            try:
+                event_list.append(
+                    models.Event(
+                        message_id=ev['message_id'],
+                        event_type=ev['event_type'],
+                        generated=timeutils.normalize_time(
+                            timeutils.parse_isotime(ev['generated'])),
+                        traits=[models.Trait(
+                                name, dtype,
+                                models.Trait.convert_value(dtype, value))
+                                for name, dtype, value in ev['traits']],
+                        raw=ev.get('raw', {}))
+                )
+            except Exception:
+                LOG.exception(_LE("Error processing event and it will be "
+                                  "dropped: %s"), ev)
+        self.event_conn.record_events(event_list)
